feac5eef0fd481ebd568439d7ca3642080f9a14c
royzce <roycemontoya77@gmail.com>
2023-02-10 18:18:22 +0800

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Checkout from "./components/Checkout";
+import Dashboard from "./components/Dashboard";
+import NavBar from "./components/NavBar";
+import { PRODUCTS_DATA as data } from "./data/products";
 
 function App() {
+  const [products, setProducts] = useState(
+    data.map((product) => {
+      return { ...product, qty: 0 };
+    })
+  );
+
+  function handleIncrement(id) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === id) {
+          return { ...product, qty: product.qty + 1 };
+        }
+        return product;
+      })
+    );
+  }
+  function handleDecrement(id) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === id && product.qty > 0) {
+          return { ...product, qty: product.qty - 1 };
+        }
+        return product;
+      })
+    );
+  }
+  function handleRemoveCart(id) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === id && product.qty > 0) {
+          return { ...product, qty: 0 };
+        }
+        return product;
+      })
+    );
+  }
+
+  function handleAddToCart(id) {
+    setProducts(
+      products.map((product) =>
+        product.id === id ? { ...product, qty: 1 } : product
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar products={products} />
+
+      <Routes>
+        <Route
+          path="/dashboard"
+          element={
+            <Dashboard
+              products={products}
+              onIncrement={handleIncrement}
+              onDecrement={handleDecrement}
+              onAddToCart={handleAddToCart}
+              onRemoveCart={handleRemoveCart}
+            />
+          }
+        />
+        <Route path="/cart-items" element={<Checkout products={products} />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Checkout.jsx
@@ -0,0 +1,86 @@
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Container } from "@mui/material";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+const Checkout = ({ products }) => {
+  const subtotal = products
+    .filter((cartItem) => cartItem.qty > 0)
+    .reduce((total, current) => total + current.qty * current.price, 0);
+
+  return (
+    <>
+      <Container sx={{ paddingBottom: "1rem" }}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }}>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Product Name</StyledTableCell>
+                <StyledTableCell align="right">Quantity</StyledTableCell>
+                <StyledTableCell align="right">Unit</StyledTableCell>
+                <StyledTableCell align="right">Price</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products
+                .filter((cartItem) => cartItem.qty > 0)
+                .map((cartItem) => (
+                  <StyledTableRow key={cartItem.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {cartItem.title}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {cartItem.qty}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      ₱ {cartItem.price}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      ₱ {+cartItem.qty * +cartItem.price}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              <TableRow>
+                <StyledTableCell></StyledTableCell>
+                <StyledTableCell align="right">
+                  <strong>Subtotal</strong>{" "}
+                </StyledTableCell>
+                <StyledTableCell align="right"></StyledTableCell>
+                <StyledTableCell align="right">
+                  <strong> ₱ {subtotal}</strong>
+                </StyledTableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Checkout;
